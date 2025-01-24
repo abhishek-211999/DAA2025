55b49ecce5da771d2b737d1b6a3ded45f0a09bab
abhishek <abhisheksabhisheks5225@gmail.com>
2025-01-24 16:44:52 +0530

    Selection Sort

---
+++ src/Btech_Lab/Section_V/SelectionSort.java
@@ -0,0 +1,32 @@
+package Btech_Lab.Section_V;
+
+public class SelectionSort {
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            swap(arr, i, minIdx);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {29, 10, 14, 37, 13};
+        selectionSort(arr);
+
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+    }
+}
